261fae9c5b31818c0d9103296b88cc9782213a08
Igor Souza <igorsouza.dev@gmail.com>
2020-04-04 12:30:22 -0300

    Added posts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,60 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import PostItem from '../components/PostItem';
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem
-      slug="/slug"
-      background="blue"
-      category="misc"
-      date="30 june, 2019"
-      timeToRead="4"
-      title="Test"
-      description="Lorem ipsum amet"
-    />
-  </Layout>
-);
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query PostList {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                background
+                category
+                title
+                description
+                date(locale: "en-US", formatString: "DD MMMM[,] YYYY")
+              }
+              timeToRead
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const posts = allMarkdownRemark.edges;
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {posts.map(
+        ({
+          node: {
+            frontmatter: { background, category, title, description, date },
+            timeToRead,
+            fields: { slug },
+          },
+        }) => (
+          <PostItem
+            slug={slug}
+            background={background}
+            category={category}
+            date={date}
+            title={title}
+            description={description}
+            timeToRead={timeToRead}
+            key={title}
+          />
+        )
+      )}
+    </Layout>
+  );
+};
 
 export default IndexPage;

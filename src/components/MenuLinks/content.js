81b5c01f3c59860eee1a0b44c594b7f279219d1a
Igor Souza <igorsouza.dev@gmail.com>
2020-04-04 08:46:09 -0300

    Added menu links

---
+++ src/components/MenuLinks/content.js
@@ -0,0 +1,12 @@
+const links = [
+  {
+    label: 'Home',
+    url: '/',
+  },
+  {
+    label: 'About me',
+    url: '/about',
+  },
+];
+
+export default links;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Profile from '../Profile';
 import SocialLinks from '../SocialLinks';
+import MenuLinks from '../MenuLinks';
 import * as S from './styles';
 
 const Sidebar = () => (
   <S.SidebarWrapper>
     <Profile />
     <SocialLinks />
+    <MenuLinks />
   </S.SidebarWrapper>
 );
 
